715f3efd4fd6f136830caba1ffe0a5e03b430079
Seth Kirksey <sethkirksey2@gmail.com>
2016-04-21 19:12:43 -0400

    recursive works

--- src/main/java/cs1302/calc/Controller.java
+++ src/main/java/cs1302/calc/Controller.java
@@ -16,10 +16,15 @@ import java.util.Scanner;
 
 public class Controller{
     String text = "";
+    String ans = "";
+    String buttonLabel = "Use Recursion";
+    IterativeMath im = new IterativeMath();
+    RecursiveMath rm = new RecursiveMath();
     @FXML public TextField display;
     @FXML public Label answer;
     @FXML public Button button;    
-   
+    @FXML public Button useThing;    
+    public static String input = "";
     @FXML public void handleButton0(ActionEvent event){
 	text += "0";
 	display.setText(text);
@@ -93,12 +98,121 @@ public class Controller{
 	display.setText(text);
 
 	}
-    @FXML public void handleButtonEquals(ActionEvent event){
-	String input;
+    @FXML public void handleButtonBackspace(ActionEvent event){
+	text = text.substring(0,text.length()-1);
+	display.setText(text);
+
+	}
+    @FXML public void handleButtonClear(ActionEvent event){
 	text = "";
+	display.setText(text);
+
+	}
+    @FXML public void handleButtonUse(ActionEvent event){
+	if(buttonLabel == "Use Recursion")buttonLabel = "Use Iteration";
+	else buttonLabel = "Use Recursion";
+	useThing.setText(buttonLabel);
+	
+	}
+    @FXML public void handleButtonEquals(ActionEvent event){
+	text += " ";
+	display.setText(text);
 	input = display.getText();
-	display.setText("");
+	text = "";
+	//display.setText(text);
 	System.out.println(input);
-	answer.setText("42");
+	//checkErrors(input);
+	doMath(input);
+	answer.setText(ans);
     }
-}
+    public void checkErrors(String input){
+	/*
+	int numChars = input.length();
+	if(input.charAt(0) == [0-9]){
+	    System.out.println("Not a number first!");
+	}
+	
+	for(int i = 0; i < numChars; i++){
+	    if(input.charAt(i) == 0){
+		
+	    }    
+	}
+	*/
+    }
+    public void doMath(String input){
+	try{
+	String copyInput = input;
+	String temp1 = input.substring(0, input.indexOf(" "));
+        copyInput = input.substring(copyInput.indexOf(" ") + 1, input.length());
+        String temp2 =  copyInput.substring(0, copyInput.indexOf(" "));
+        copyInput = copyInput.substring(copyInput.indexOf(" ") + 1, copyInput.length());
+        String temp3 =  copyInput.substring(0, copyInput.indexOf(" "));
+        int part1 = Integer.parseInt(temp1);
+        int part2 = Integer.parseInt(temp3);
+	//ans = Integer.toString(part1 + part2);
+	int l = part1;
+	int r = part2;
+       
+	if(useThing.getText().equals("Use Recursion")){//This is for the iteration methods
+	    // System.out.println("bar!");
+	    if(temp2.equals("+")){
+		//System.out.println("foo!");
+		ans = Integer.toString(im.add(l,r));
+	    }
+	    if(temp2.equals("-")){
+		ans = Integer.toString(im.sub(l,r));
+	    }
+	    if(temp2.equals("*")){
+		im.mul(l,r);
+	    }
+	    if(temp2.equals("/")){
+		im.div(l,r);
+	    }
+	    if(temp2.equals("!")){
+		im.fac(l);
+	    }
+	    if(temp2.equals("^")){
+		im.pow(l,r);
+	    }
+	    if(temp2.equals("«")){
+		
+	    }
+	    if(temp2.equals("»")){
+		
+		
+	    }
+	}
+	if(useThing.getText().equals("Use Iteration")){//This is for the recursionn methods
+	    
+	    if(temp2.equals("+")){
+		//System.out.println("foo");
+		ans = Integer.toString(rm.add(l,r));
+	    }
+	    if(temp2.equals("-")){
+		ans = Integer.toString(rm.sub(l,r));
+	    }
+	    if(temp2.equals("*")){
+		ans = Integer.toString(rm.mul(l,r));
+	    }
+	    if(temp2.equals("/")){
+		ans = Integer.toString(rm.div(l,r));
+	    }
+	    if(temp2.equals("!")){
+		rm.fac(l);
+	    }
+	    if(temp2.equals("^")){
+		rm.pow(l,r);
+	    }
+	    if(temp2.equals("«")){
+		
+	    }
+	    if(temp2.equals("»")){
+		
+	    }
+	}
+    }
+    catch(Exception e){
+	System.out.println("Dafuq");
+    }
+    }//doMath
+}//Controller
